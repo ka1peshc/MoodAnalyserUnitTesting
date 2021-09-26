c23384d22d0eec8dc56c6a3cf1310ea73e91c761
ka1peshc <kalpesh111997@gmail.com>
2021-09-26 12:56:04 +0530

    Added Default Constructor and Parameter Constructor

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -2,22 +2,40 @@
 
 namespace MoodAnalyser
 {
-    class Program
+    public class Program
     {
-        public string moodAnalyser(string msg)
+        private readonly string message;
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Program()
+        {
+        }
+        /// <summary>
+        /// Parameter Constructor to assign message
+        /// </summary>
+        /// <param name="msg"></param>
+        public Program(string msg)
+        {
+            this.message = msg;
+        }
+        
+        public string moodAnalyser()
         {
             
-            var tempArr = msg.ToLower().Split(" ");
+            var tempArr = message.ToLower().Split(" ");
             string result="";
             for(int i = 0; i < tempArr.Length; i++)
             {
                 if (tempArr[i].Equals("sad"))
                 {
                     result = "SAD";
+                    break;
                 }
                 else if (tempArr[i].Equals("happy") || tempArr[i].Equals("any"))
                 {
                     result = "HAPPY";
+                    break;
                 }
                 else
                 {
@@ -26,11 +44,11 @@ namespace MoodAnalyser
             }
             return result;
         }
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
-            Program Moodanalyser = new Program();
-            string result = Moodanalyser.moodAnalyser("I am Happy");
+            Program Moodanalyser = new Program("I am Happy");
+            string result = Moodanalyser.moodAnalyser();
             Console.WriteLine(result);
         }
     }
