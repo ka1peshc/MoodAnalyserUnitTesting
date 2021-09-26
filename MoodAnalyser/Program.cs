90bd44a94d146dd485462a4249009585b04f2e14
ka1peshc <kalpesh111997@gmail.com>
2021-09-26 12:24:48 +0530

    Analysing mood based on word happy or sad or any

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        public string moodAnalyser(string msg)
+        {
+            
+            var tempArr = msg.ToLower().Split(" ");
+            string result="";
+            for(int i = 0; i < tempArr.Length; i++)
+            {
+                if (tempArr[i].Equals("sad"))
+                {
+                    result = "SAD";
+                }
+                else if (tempArr[i].Equals("happy") || tempArr[i].Equals("any"))
+                {
+                    result = "HAPPY";
+                }
+                else
+                {
+                    result = null;
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program Moodanalyser = new Program();
+            string result = Moodanalyser.moodAnalyser("I am Happy");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+learning unit test coding
