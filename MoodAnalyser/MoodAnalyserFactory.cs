fd3a6f03fb61a326ce4f0971291e2e466271effd
ka1peshc <kalpesh111997@gmail.com>
2021-10-01 12:26:45 +0530

    Added CreateMoodAnalyser function with parameter

--- MoodAnalyser/MoodAnalyserFactory.cs
+++ MoodAnalyser/MoodAnalyserFactory.cs
@@ -32,5 +32,33 @@ namespace MoodAnalyser
             }
 
         }
+        /// <summary>
+        /// CreateMoodAnalyse method to create object of MoodAnalyserClass
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyserClass);
+            if(type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new CustomMoodAnalyser(CustomMoodAnalyser.ExceptionType.No_Such_Method, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new CustomMoodAnalyser(CustomMoodAnalyser.ExceptionType.No_Such_Class, "Class not found");
+            }
+        }
     }
 }
